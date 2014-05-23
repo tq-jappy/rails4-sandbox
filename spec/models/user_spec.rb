9bb3599d2ee2e924fc843fb1e3ec9254ac4f5a15
Takumi Endo <tq-ed@v7.com>
2014-05-24 01:34:54 +0900

    add first user test.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,21 @@ require 'spec_helper'
 
 describe "User" do
 
+  it "is valid with a username and email" do
+    user = User.new(
+      username: "allen",
+      password: "password",
+      email: "allen@rails4.sandbox.com"
+    )
+    expect(user).to be_valid
+  end
+
+  it "is invalid without an email" do
+    user = User.new(
+      username: "allen",
+      password: "password",
+      email: nil
+    )
+    expect(user).to have(1).errors_on(:email)
+  end
 end
